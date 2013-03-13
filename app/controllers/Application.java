4a26e3be42f9ef6d222d6df39987b90fb512ec79
nishinaka shinya <n-shinya@flect.co.jp>
2013-03-13 21:55:12 +0900

    Add parameter.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,17 +16,16 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
+    public static void index(String keyword) {
 
         Map<String, String> params = new HashMap<String, String>();
         params.put("Service", "AWSECommerceService");
         params.put("Operation", "ItemSearch");
-        params.put("Keywords", "4902370518986");
-        params.put("ResponseGroup", "Medium");
+        params.put("Keywords", keyword);
+        params.put("ResponseGroup", "Large");
         params.put("SearchIndex", "All");
 
         HttpResponse res = WS.url(new SignedRequest().sign(params)).get();
-        System.out.println(res.getString());
         Document doc = res.getXml();
         Node node = XPath.selectNode("/ItemSearchResponse/Items/Item", doc);
         String title = XPath.selectText("ItemAttributes/Title", node);
--- app/helper/SignedRequest.java
+++ app/helper/SignedRequest.java
@@ -25,110 +25,110 @@ import play.mvc.Controller;
  * .com/AWSECommerceService/latest/DG/index.html?AuthJavaSampleSig2.html
  */
 public class SignedRequest {
-	
-	private static final String UTF8_CHARSET = "UTF-8";
-	private static final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
-	private static final String REQUEST_URI = "/onca/xml";
-	private static final String REQUEST_METHOD = "GET";
+    
+    private static final String UTF8_CHARSET = "UTF-8";
+    private static final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+    private static final String REQUEST_URI = "/onca/xml";
+    private static final String REQUEST_METHOD = "GET";
 
-	private String endpoint = "ecs.amazonaws.jp";
-	private String awsAccessKeyId;
-	private String awsSecretKey;
-	private String associateTag;
+    private String endpoint = "ecs.amazonaws.jp";
+    private String awsAccessKeyId;
+    private String awsSecretKey;
+    private String associateTag;
 
-	private SecretKeySpec secretKeySpec = null;
-	private Mac mac = null;
+    private SecretKeySpec secretKeySpec = null;
+    private Mac mac = null;
 
-	public SignedRequest() {
-		try {
-			this.awsAccessKeyId = Play.configuration.getProperty("awsAccessKeyId");
-			this.awsSecretKey = Play.configuration.getProperty("awsSecretKey");
-			this.associateTag = Play.configuration.getProperty("associateTag");
-			byte[] secretyKeyBytes = awsSecretKey.getBytes(UTF8_CHARSET);
-			secretKeySpec = new SecretKeySpec(secretyKeyBytes, HMAC_SHA256_ALGORITHM);
-			mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
-			mac.init(secretKeySpec);			
-		} catch(Exception e) {
-			throw new JavaExecutionException(e);
-		}
-	}
+    public SignedRequest() {
+        try {
+            this.awsAccessKeyId = Play.configuration.getProperty("awsAccessKeyId");
+            this.awsSecretKey = Play.configuration.getProperty("awsSecretKey");
+            this.associateTag = Play.configuration.getProperty("associateTag");
+            byte[] secretyKeyBytes = awsSecretKey.getBytes(UTF8_CHARSET);
+            secretKeySpec = new SecretKeySpec(secretyKeyBytes, HMAC_SHA256_ALGORITHM);
+            mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(secretKeySpec);            
+        } catch(Exception e) {
+            throw new JavaExecutionException(e);
+        }
+    }
 
-	public String sign(Map<String, String> params) {
-		params.put("AWSAccessKeyId", awsAccessKeyId);
-		params.put("AssociateTag", associateTag);
-		params.put("Timestamp", timestamp());
+    public String sign(Map<String, String> params) {
+        params.put("AWSAccessKeyId", awsAccessKeyId);
+        params.put("AssociateTag", associateTag);
+        params.put("Timestamp", timestamp());
 
-		SortedMap<String, String> sortedParamMap = new TreeMap<String, String>(
-			params);
-		String canonicalQS = canonicalize(sortedParamMap);
-		String toSign = REQUEST_METHOD + "\n" + endpoint + "\n" + REQUEST_URI
-			+ "\n" + canonicalQS;
+        SortedMap<String, String> sortedParamMap = new TreeMap<String, String>(
+            params);
+        String canonicalQS = canonicalize(sortedParamMap);
+        String toSign = REQUEST_METHOD + "\n" + endpoint + "\n" + REQUEST_URI
+            + "\n" + canonicalQS;
 
-		String hmac = hmac(toSign);
-		String sig = percentEncodeRfc3986(hmac);
-		String url = "http://" + endpoint + REQUEST_URI + "?" + canonicalQS
-			+ "&Signature=" + sig;
+        String hmac = hmac(toSign);
+        String sig = percentEncodeRfc3986(hmac);
+        String url = "http://" + endpoint + REQUEST_URI + "?" + canonicalQS
+            + "&Signature=" + sig;
 
-		return url;
-	}
+        return url;
+    }
 
-	private String hmac(String stringToSign) {
-		String signature = null;
-		byte[] data;
-		byte[] rawHmac;
-		try {
-			data = stringToSign.getBytes(UTF8_CHARSET);
-			rawHmac = mac.doFinal(data);
-			Base64 encoder = new Base64(0);
-			signature = new String(encoder.encode(rawHmac));
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(UTF8_CHARSET + " is unsupported!", e);
-		}
-		return signature;
-	}
+    private String hmac(String stringToSign) {
+        String signature = null;
+        byte[] data;
+        byte[] rawHmac;
+        try {
+            data = stringToSign.getBytes(UTF8_CHARSET);
+            rawHmac = mac.doFinal(data);
+            Base64 encoder = new Base64(0);
+            signature = new String(encoder.encode(rawHmac));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(UTF8_CHARSET + " is unsupported!", e);
+        }
+        return signature;
+    }
 
-	private String timestamp() {
-		String timestamp = null;
-		Calendar cal = Calendar.getInstance();
-		DateFormat dfm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
-		timestamp = dfm.format(cal.getTime());
-		return timestamp;
-	}
+    private String timestamp() {
+        String timestamp = null;
+        Calendar cal = Calendar.getInstance();
+        DateFormat dfm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
+        timestamp = dfm.format(cal.getTime());
+        return timestamp;
+    }
 
-	private String canonicalize(SortedMap<String, String> sortedParamMap) {
-		if (sortedParamMap.isEmpty()) {
-			return "";
-		}
-	
-		StringBuffer buffer = new StringBuffer();
-		Iterator<Map.Entry<String, String>> iter = sortedParamMap.entrySet()
-				.iterator();
-	
-		while (iter.hasNext()) {
-			Map.Entry<String, String> kvpair = iter.next();
-			buffer.append(percentEncodeRfc3986(kvpair.getKey()));
-			buffer.append("=");
-			buffer.append(percentEncodeRfc3986(kvpair.getValue()));
-			if (iter.hasNext()) {
-				buffer.append("&");
-			}
-		}
-		String cannoical = buffer.toString();
-		return cannoical;
-	}
+    private String canonicalize(SortedMap<String, String> sortedParamMap) {
+        if (sortedParamMap.isEmpty()) {
+            return "";
+        }
+    
+        StringBuffer buffer = new StringBuffer();
+        Iterator<Map.Entry<String, String>> iter = sortedParamMap.entrySet()
+                .iterator();
+    
+        while (iter.hasNext()) {
+            Map.Entry<String, String> kvpair = iter.next();
+            buffer.append(percentEncodeRfc3986(kvpair.getKey()));
+            buffer.append("=");
+            buffer.append(percentEncodeRfc3986(kvpair.getValue()));
+            if (iter.hasNext()) {
+                buffer.append("&");
+            }
+        }
+        String cannoical = buffer.toString();
+        return cannoical;
+    }
 
-	private String percentEncodeRfc3986(String s) {
-		if(s == null) {
-			return "";
-		}
-		String out;	
-		try {
-			out = URLEncoder.encode(s, UTF8_CHARSET).replace("+", "%20")
-					.replace("*", "%2A").replace("%7E", "~");
-		} catch (UnsupportedEncodingException e) {
-			out = s;
-		}
-		return out;
-	}
+    private String percentEncodeRfc3986(String s) {
+        if(s == null) {
+            return "";
+        }
+        String out;    
+        try {
+            out = URLEncoder.encode(s, UTF8_CHARSET).replace("+", "%20")
+                    .replace("*", "%2A").replace("%7E", "~");
+        } catch (UnsupportedEncodingException e) {
+            out = s;
+        }
+        return out;
+    }
 }
