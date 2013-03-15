b9ce11a63395a461625345f3a90073db955860a5
nishinaka shinya <n-shinya@flect.co.jp>
2013-03-15 20:34:44 +0900

    Response basic product info.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,6 @@ package controllers;
 
 import helper.SignedRequest;
 
-import java.awt.print.Book;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +13,8 @@ import play.libs.WS.HttpResponse;
 import play.libs.XPath;
 import play.mvc.Controller;
 
+import com.google.gson.Gson;
+
 public class Application extends Controller {
 
     public static void index(String keyword) {
@@ -22,14 +23,19 @@ public class Application extends Controller {
         params.put("Service", "AWSECommerceService");
         params.put("Operation", "ItemSearch");
         params.put("Keywords", keyword);
-        params.put("ResponseGroup", "Large");
+        params.put("ResponseGroup", "Medium");
         params.put("SearchIndex", "All");
 
         HttpResponse res = WS.url(new SignedRequest().sign(params)).get();
+        System.out.println(res.getString());
         Document doc = res.getXml();
         Node node = XPath.selectNode("/ItemSearchResponse/Items/Item", doc);
-        String title = XPath.selectText("ItemAttributes/Title", node);
-        renderJSON("{'Title':" + title + "}");
+        
+        Map<String, String> item = new HashMap<String, String>();
+        item.put("title", XPath.selectText("ItemAttributes/Title", node));
+        item.put("image", XPath.selectText("MediumImage/URL", node));
+        item.put("price", XPath.selectText("OfferSummary/LowestNewPrice/Amount", node));
+        renderJSON(new Gson().toJson(item));
     }
 
 }
